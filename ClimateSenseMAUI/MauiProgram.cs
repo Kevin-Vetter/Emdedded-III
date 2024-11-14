b0f40d80c2bd26a10906e71986a3b95440f6b33c
failbreak <max.larsen@live.dk>
2024-11-14 09:30:33 +0100

    Please dont crash

--- ClimateSenseMAUI/MauiProgram.cs
+++ ClimateSenseMAUI/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Syncfusion.Maui.Core.Hosting;
 
 namespace ClimateSenseMAUI
 {
@@ -9,6 +10,7 @@ namespace ClimateSenseMAUI
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .ConfigureSyncfusionCore()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
