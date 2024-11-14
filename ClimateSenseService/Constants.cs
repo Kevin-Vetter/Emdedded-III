71f75b921f54d219f16faf7465e7560744cac004
Failbreak <GetShreked@NotHappening>
2024-11-14 19:01:34 +0100

    Merged from master added migration

--- ClimateSenseService/Constants.cs
+++ ClimateSenseService/Constants.cs
@@ -3,5 +3,5 @@ namespace ClimateSenseService;
 public class Constants
 {
     public static string BaseUrl = "https://6t0pcctr-5117.euw.devtunnels.ms";
-    public static string Endpoint = "todoitems";
+    public static string Endpoint = "";
 }
