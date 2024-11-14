db042bbb2aed63096f95e0121a4c417bda2b96a2
Kevin Vetter <kevinmvetter@gmail.com>
2024-11-14 20:35:23 +0100

    polly setup

---
+++ ClimateSenseMAUI/Policies/ClientPolicy.cs
@@ -0,0 +1,21 @@
+﻿using Polly;
+using Polly.Retry;
+using System.Diagnostics;
+
+namespace ClimateSenseMAUI.Policies
+{
+
+
+    public class ClientPolicy
+    {
+        public AsyncRetryPolicy<HttpResponseMessage> ExponentialHttpRetry { get; }
+
+        public ClientPolicy()
+        {
+            ExponentialHttpRetry = Policy.HandleResult<HttpResponseMessage>(
+               res => !res.IsSuccessStatusCode)
+                .WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+        }
+
+    }
+}
