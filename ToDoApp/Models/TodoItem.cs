b7ab21f8a606b4648db7306314fd635ee2605489
Kevin Vetter <kevinmvetter@gmail.com>
2024-11-14 08:45:44 +0100

    revert

---
+++ ToDoApp/Models/TodoItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+    public class TodoItem
+    {
+        public int Id {  get; set; }
+        public string Description { get; set; }
+        public DateTime CreateTime { get; set; }
+        public PriorityLevel Priority {  get; set; }
+        public bool Completed { get; set; }
+
+
+    }
+}
