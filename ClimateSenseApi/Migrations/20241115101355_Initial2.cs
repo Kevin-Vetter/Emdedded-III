8d7649fee303a3fb4a1c1fac6001816d9373dabc
Kevin Vetter <kevinmvetter@gmail.com>
2024-11-18 09:02:03 +0100

    commit

---
+++ ClimateSenseApi/Migrations/20241115101355_Initial2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClimateSenseApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Measurements",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Timestamp = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Device = table.Column<string>(type: "TEXT", nullable: false),
+                    MeasurementType = table.Column<int>(type: "INTEGER", nullable: false),
+                    Location = table.Column<string>(type: "TEXT", nullable: false),
+                    Value = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Measurements", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Measurements");
+        }
+    }
+}
--- ClimateSenseServices/Constants.cs
+++ ClimateSenseServices/Constants.cs
@@ -2,6 +2,6 @@ namespace ClimateSenseServices;
 
 public class Constants
 {
-    public static string BaseUrl = "https://4hnwj87h-6001.euw.devtunnels.ms";
+    public static string BaseUrl = "https://f6g2d4lr-6001.euw.devtunnels.ms/";
     public static string Endpoint = "Measurement";
 }
